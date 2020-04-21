f002a7cf2d8ee438d85ede8c2fd692f7b9d9015d
Sreekar <tankalasrkr@gmail.com>
2020-04-22 01:32:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import dash_bootstrap_components as dbc
 app = dash.Dash(__name__)
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
